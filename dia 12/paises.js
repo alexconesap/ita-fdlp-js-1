f41a3ef47f2f247504c4263a8ba1ba4c45727c93
YAKUMA <info@yakuma.com>
2022-05-10 15:58:03 +0200

    Reproducción de himnos

--- dia 12/paises.js
+++ dia 12/paises.js
@@ -1,60 +1,66 @@
 class Country {
-    #iso_code;
-    #name;
-    #population;
+  #iso_code;
+  #name;
+  #population;
+  #playing = false;
 
-    constructor(iso_code, name, population = 0) {
-        this.#iso_code = iso_code;
-        this.#name = name;
-        this.#population = population;
-    }
+  constructor(iso_code, name, population = 0) {
+    this.#iso_code = iso_code;
+    this.#name = name;
+    this.#population = population;
+  }
 
-    play() {
-        let himno = new Audio(this.#iso_code + '.mp3');
-        himno.play();
+  play_hymn() {
+    if (!this.#playing) {
+      this.#playing = true;
+      let arxiu_so = this.#iso_code + ".ogg";
+      let himno = new Audio(arxiu_so);
+      himno.play();
     }
+  }
 
-    getIsoCode() {
-        return this.#iso_code;
-    }
+  getIsoCode() {
+    return this.#iso_code;
+  }
 }
 
 class Countries {
-    #paises;
+  #paises;
 
-    constructor() {
-        this.#paises = [];
-    }
+  constructor() {
+    this.#paises = [];
+  }
 
-    mete (country_obj) {
-        if (this.get(country_obj.getIsoCode())) {
-            return;
-        }
-        this.#paises.push(country_obj);
+  mete(country_obj) {
+    if (this.get(country_obj.getIsoCode())) {
+      return;
     }
+    this.#paises.push(country_obj);
+  }
 
-    get (iso_code) {
-        return this.#paises.find(
-            (e) => e.getIsoCode() == iso_code
-        );
-    }
+  get(iso_code) {
+    return this.#paises.find(
+        (e) => e.getIsoCode() == iso_code
+    );
+  }
 
-    list() {
-        return this.#paises;
-    }
+  list() {
+    return this.#paises;
+  }
 }
 
+// Creamos un par de objetos de tipo Country
 let france = new Country("FR", "France", 59_000_000);
-let andorra = new Country("AN", "Andorra", 150_000);
+let it = new Country("IT", "Italy", 50_000_000);
 
+// Creamos una colección de países (Countries)
 let paises = new Countries();
 paises.mete(france);
-paises.mete(andorra);
-paises.mete(andorra);
-paises.mete(andorra);
+paises.mete(it);
+paises.mete(it);
 console.log(paises.list());
 
-console.log(paises.get("FR"));
-
-//france.play();
-
+function sample_play_hymn() {
+  let fr_obj = paises.get("FR");
+  fr_obj && fr_obj.play_hymn();
+}
